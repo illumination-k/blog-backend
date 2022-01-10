15e3d8a1581f868777096ebc07ebd2c2faf7ce0e
illumination-k <illumination.k.27@gmail.com>
2022-01-11 01:01:47 +0900

    fix params of search api

--- src/server/route/posts/search.rs
+++ src/server/route/posts/search.rs
@@ -11,27 +11,36 @@ use crate::text_engine::{
 
 #[derive(Debug, Deserialize)]
 pub struct SearchQueryParams {
-    query: String,
+    query: Option<String>,
+    limit: Option<usize>,
 }
 
 #[get("/posts/search")]
 async fn search_posts(index: web::Data<Index>, req: HttpRequest) -> HttpResponse {
     let index = index.into_inner();
-    let query = web::Query::<SearchQueryParams>::from_query(req.query_string())
-        .unwrap()
-        .query
-        .to_owned();
+    let params = web::Query::<SearchQueryParams>::from_query(req.query_string())
+        .unwrap();
+    
     let fb = FieldGetter::new(index.schema());
     let fields = [PostField::Title, PostField::Description, PostField::RawText]
         .into_iter()
         .map(|pf| fb.get_field(pf))
         .collect_vec();
+    
+    let limit = if let Some(limit) = params.limit.to_owned() {
+        limit
+    } else {
+        10
+    };
 
-    let docs = search(&query.to_owned().to_lowercase(), fields, 10, index.deref()).unwrap();
-
-    let docs = docs
-        .iter()
-        .map(|doc| index.schema().to_named_doc(doc))
-        .collect_vec();
+    let docs = if let Some(query) = params.query.to_owned() {
+        search(&query.to_owned().to_lowercase(), fields, limit, index.deref())
+            .unwrap()
+            .iter()
+            .map(|doc| index.schema().to_named_doc(doc))
+            .collect_vec()
+    } else {
+        Vec::new()
+    };
     HttpResponse::Ok().json(docs)
 }
