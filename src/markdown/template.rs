4229d5ecf89ac883d86ab489428093f7119ef76d
illumination-k <illumination.k.27@gmail.com>
2022-01-11 19:58:41 +0900

    update template

--- src/markdown/template.rs
+++ src/markdown/template.rs
@@ -4,11 +4,11 @@ pub fn template() -> String {
     let uuid = Uuid::new_v4().to_string();
     let template = format!(
         "---
-id: {}
+uuid: {}
 title:
 description:
-category: misc
 lang: ja
+category: misc
 ---
 
 ## TL;DR
