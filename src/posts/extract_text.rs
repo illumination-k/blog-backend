f101f50cca8cc45e35cb02ab50e385a3b250abde
illumination-k <illumination.k.27@gmail.com>
2022-04-13 11:40:47 +0900

    remove htmltags and comments in raw_text

--- src/posts/extract_text.rs
+++ src/posts/extract_text.rs
@@ -1,12 +1,78 @@
+use html5ever::{
+    buffer_queue::BufferQueue,
+    tendril::ByteTendril,
+    tokenizer::{Token, TokenSink, TokenSinkResult, Tokenizer},
+};
 use pulldown_cmark::{Event, Options, Parser};
+use Token::CharacterTokens;
+
+#[derive(Debug, PartialEq)]
+enum State {
+    Ready,
+    InHtml,
+}
+
+#[derive(Debug)]
+struct Sink(Vec<Token>);
+
+impl TokenSink for Sink {
+    type Handle = ();
+
+    fn process_token(&mut self, token: Token, _line_number: u64) -> TokenSinkResult<()> {
+        self.0.push(token);
+        TokenSinkResult::Continue
+    }
+}
+
+fn extract_inner_text(html: &mut BufferQueue) -> Vec<String> {
+    let mut s = vec![];
+
+    let mut tokenizer = Tokenizer::new(Sink(Vec::new()), Default::default());
+    let _ = tokenizer.feed(html);
+    tokenizer.end();
+
+    for token in tokenizer.sink.0.iter() {
+        match token {
+            CharacterTokens(tendri) => {
+                let inner = tendri.to_string().trim().to_string();
+                if inner.is_empty() {
+                    continue;
+                }
+                s.push(inner);
+            }
+            _ => {}
+        }
+    }
+
+    s
+}
 
 pub fn extract_text(markdown_text: &str) -> String {
+    let mut state = State::Ready;
     let mut s = vec![];
-    let parser = Parser::new_ext(markdown_text, Options::empty());
+    let mut html_values = BufferQueue::new();
 
+    let parser = Parser::new_ext(markdown_text, Options::empty());
+    //let html_parser = parse_fragment(TokenSink, ParseOpts::default(), context_name, context_attrs)
     for e in parser {
         match e {
-            Event::Text(text) => s.push(text.to_string()),
+            Event::Text(text) => {
+                if state == State::InHtml {
+                    let inner_values = extract_inner_text(&mut html_values);
+                    s.extend(inner_values.into_iter());
+                    state = State::Ready;
+                }
+
+                s.push(text.to_string());
+            }
+            Event::Html(html) => {
+                if state == State::Ready {
+                    state = State::InHtml;
+                }
+                let chunk =
+                    unsafe { ByteTendril::from_byte_slice_without_validating(html.as_bytes()) };
+                html_values.push_back(chunk.try_reinterpret().unwrap());
+            }
             _ => continue,
         }
     }
@@ -39,5 +105,24 @@ fn main() {
 
 Some Codes"#
         )
+    }
+
+    #[test]
+    fn test_ignore_comment() {
+        let markdown_text = r#"
+<!-- comments -->
+
+<!--
+multi line
+comments
+-->
+
+<div><p>inner</p></div>
+
+## TEST
+"#;
+        let text = extract_text(markdown_text);
+        let expected = "inner\nTEST";
+        assert_eq!(text, expected);
     }
 }
